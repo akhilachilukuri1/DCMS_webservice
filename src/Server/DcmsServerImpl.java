8bd3a2d2740386956455548bc26651db34437c0a
VigneswarM <imvigneswar@gmail.com>
2018-07-06 09:08:31 -0400

    servImpl changes

--- src/Server/DcmsServerImpl.java
+++ src/Server/DcmsServerImpl.java
@@ -1,14 +1,9 @@
 package Server;
 
-
-
-
 import java.io.IOException;
-
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.logging.Level;
-
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
@@ -26,10 +21,9 @@ import Models.Teacher;
  * Locations
  *
  */
-@WebService(endpointInterface = "Server.Dcms",portName="DcmsPort",serviceName="DcmsService")
-//@SOAPBinding(style = Style.RPC)
-public class DcmsServerImpl implements Dcms  {
-	
+@WebService(endpointInterface = "Server.Dcms", portName = "DcmsPort", serviceName = "DcmsService")
+// @SOAPBinding(style = Style.RPC)
+public class DcmsServerImpl implements Dcms {
 
 	LogManager logManager;
 	ServerUDP serverUDP;
@@ -40,8 +34,7 @@ public class DcmsServerImpl implements Dcms  {
 	String recordsCount;
 	String location;
 	
-	public DcmsServerImpl()
-	{
+	public DcmsServerImpl() {
 		super();
 	}
 
@@ -52,9 +45,7 @@ public class DcmsServerImpl implements Dcms  {
 		serverUDP.start();
 		location = loc.toString();
 		setIPAddress(loc);
-	}
-
-
+		}
 
 	private void setIPAddress(ServerCenterLocation loc) {
 		switch (loc) {
@@ -70,7 +61,9 @@ public class DcmsServerImpl implements Dcms  {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see Server.Dcms#createTRecord(java.lang.String, java.lang.String)
 	 */
 
@@ -86,19 +79,37 @@ public class DcmsServerImpl implements Dcms  {
 		String phone = temp[3];
 		String specialization = temp[4];
 		String location = temp[5];
-		Teacher teacherObj = new Teacher(managerID, teacherID, firstName, lastname,
-				address, phone, specialization, location);
+		Teacher teacherObj = new Teacher(managerID, teacherID, firstName, lastname, address, phone, specialization,
+				location);
 		String key = lastname.substring(0, 1);
-		String message = addRecordToHashMap(key, teacherObj, null);
-		System.out.println("teacher is added " + teacherObj + " with this key " + key
-				+ " by Manager " + managerID);
-		logManager.logger.log(Level.INFO, "Teacher record created " + teacherID
-				+ " by Manager : " + managerID);
+		// String message = addRecordToHashMap(key, teacherObj, null);
+
+		String message = "Error";
+		if (teacherObj != null) {
+			List<Record> recordList = recordsMap.get(key);
+			if (recordList != null) {
+				recordList.add(teacherObj);
+			} else {
+				List<Record> records = new ArrayList<Record>();
+				records.add(teacherObj);
+				recordList = records;
+			}
+			recordsMap.put(key, recordList);
+			message = "success";
+		}
+
+		if (message.equals("success")) {
+
+			System.out.println("teacher is added " + teacherObj + " with this key " + key + " by Manager " + managerID);
+			logManager.logger.log(Level.INFO, "Teacher record created " + teacherID + " by Manager : " + managerID);
+		}
 		return teacherID;
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see Server.Dcms#createSRecord(java.lang.String, java.lang.String)
 	 */
 
@@ -110,19 +121,35 @@ public class DcmsServerImpl implements Dcms  {
 		String firstName = temp[0];
 		String lastName = temp[1];
 		String CoursesRegistered = temp[2];
-		List<String> courseList = putCoursesinList(CoursesRegistered);
+		//List<String> courseList = _operations.putCoursesinList(CoursesRegistered);
+		String[] courses = CoursesRegistered.split("//");
+		ArrayList<String> courseList = new ArrayList<>();
+		for (String course : courses)
+			courseList.add(course);
 		String status = temp[3];
 		String statusDate = temp[4];
 		String studentID = "SR" + (++studentCount);
-		Student studentObj = new Student(managerID, studentID, firstName, lastName,
-				courseList, status, statusDate);
+		Student studentObj = new Student(managerID, studentID, firstName, lastName, courseList, status, statusDate);
 		String key = lastName.substring(0, 1);
-		String message = addRecordToHashMap(key, null, studentObj);
+		// String message = addRecordToHashMap(key, null, studentObj);
+		String message = "Error";
+		if (studentObj != null) {
+			List<Record> recordList = recordsMap.get(key);
+			if (recordList != null) {
+				recordList.add(studentObj);
+			} else {
+				List<Record> records = new ArrayList<Record>();
+				records.add(studentObj);
+				recordList = records;
+			}
+			recordsMap.put(key, recordList);
+			message = "success";
+		}
+
 		if (message.equals("success")) {
-			System.out.println(" Student is added " + studentObj + " with this key "
-					+ key + " by Manager " + managerID);
-			logManager.logger.log(Level.INFO, "Student record created " + studentID
-					+ " by manager : " + managerID);
+			System.out
+					.println(" Student is added " + studentObj + " with this key " + key + " by Manager " + managerID);
+			logManager.logger.log(Level.INFO, "Student record created " + studentID + " by manager : " + managerID);
 		}
 		return studentID;
 	}
@@ -141,39 +168,22 @@ public class DcmsServerImpl implements Dcms  {
 	 *            which are received the respective functions.
 	 * 
 	 */
-	//@WebMethod(exclude=true)
-	public synchronized String addRecordToHashMap(String key, Teacher teacher,
-			Student student) {
-		String message = "Error";
-		if (teacher != null) {
-			List<Record> recordList = recordsMap.get(key);
-			if (recordList != null) {
-				recordList.add(teacher);
-			} else {
-				List<Record> records = new ArrayList<Record>();
-				records.add(teacher);
-				recordList = records;
-			}
-			recordsMap.put(key, recordList);
-			message = "success";
-		}
-
-		if (student != null) {
-			List<Record> recordList = recordsMap.get(key);
-			if (recordList != null) {
-				recordList.add(student);
-			} else {
-				List<Record> records = new ArrayList<Record>();
-				records.add(student);
-				recordList = records;
-			}
-			recordsMap.put(key, recordList);
-			message = "success";
-		}
-
-		return message;
-	}
-
+	// @WebMethod(exclude=true)
+	/*
+	 * public synchronized String addRecordToHashMap(String key, Teacher teacher,
+	 * Student student) { String message = "Error"; if (teacher != null) {
+	 * List<Record> recordList = recordsMap.get(key); if (recordList != null) {
+	 * recordList.add(teacher); } else { List<Record> records = new
+	 * ArrayList<Record>(); records.add(teacher); recordList = records; }
+	 * recordsMap.put(key, recordList); message = "success"; }
+	 * 
+	 * if (student != null) { List<Record> recordList = recordsMap.get(key); if
+	 * (recordList != null) { recordList.add(student); } else { List<Record> records
+	 * = new ArrayList<Record>(); records.add(student); recordList = records; }
+	 * recordsMap.put(key, recordList); message = "success"; }
+	 * 
+	 * return message; }
+	 */
 	/**
 	 *
 	 * returns the current server record count
@@ -189,7 +199,9 @@ public class DcmsServerImpl implements Dcms  {
 		return count;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see Server.Dcms#getRecordCount()
 	 */
 
@@ -207,9 +219,7 @@ public class DcmsServerImpl implements Dcms  {
 				recordCount = loc + " " + getCurrServerCnt();
 			} else {
 				try {
-					req[counter] = new UDPRequestProvider(
-							DcmsServer.serverRepo.get(loc), "GET_RECORD_COUNT",
-							null);
+					req[counter] = new UDPRequestProvider(DcmsServer.serverRepo.get(loc), "GET_RECORD_COUNT", null);
 				} catch (IOException e) {
 					logManager.logger.log(Level.SEVERE, e.getMessage());
 				}
@@ -228,13 +238,15 @@ public class DcmsServerImpl implements Dcms  {
 		return recordCount;
 	}
 
-	/* (non-Javadoc)
-	 * @see Server.Dcms#editRecord(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Server.Dcms#editRecord(java.lang.String, java.lang.String,
+	 * java.lang.String, java.lang.String)
 	 */
 
 	@Override
-	public String editRecord(String managerID, String recordID, String fieldname,
-			String newvalue) {
+	public String editRecord(String managerID, String recordID, String fieldname, String newvalue) {
 		String type = recordID.substring(0, 2);
 		if (type.equals("TR")) {
 			return editTRRecord(managerID, recordID, fieldname, newvalue);
@@ -245,26 +257,25 @@ public class DcmsServerImpl implements Dcms  {
 		return "Operation not performed!";
 	}
 
-	/* (non-Javadoc)
-	 * @see Server.Dcms#transferRecord(java.lang.String, java.lang.String, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Server.Dcms#transferRecord(java.lang.String, java.lang.String,
+	 * java.lang.String)
 	 */
 	@Override
-	public String transferRecord(String ManagerID, String recordID,
-			String remoteCenterServerName) {
+	public String transferRecord(String ManagerID, String recordID, String remoteCenterServerName) {
 		String type = recordID.substring(0, 2);
 		UDPRequestProvider req = null;
 		try {
-			System.out
-					.println("Transferring record to :: " + remoteCenterServerName);
+			System.out.println("Transferring record to :: " + remoteCenterServerName);
 			Record record = getRecordForTransfer(recordID);
 			if (record == null) {
 				return "RecordID unavailable!";
-			}else if(remoteCenterServerName.equals(this.location)) {
-				return "Please enter a valid location to transfer. The record is already present in "+location;
+			} else if (remoteCenterServerName.equals(this.location)) {
+				return "Please enter a valid location to transfer. The record is already present in " + location;
 			}
-			req = new UDPRequestProvider(
-					DcmsServer.serverRepo.get(remoteCenterServerName),
-					"TRANSFER_RECORD", record);
+			req = new UDPRequestProvider(DcmsServer.serverRepo.get(remoteCenterServerName), "TRANSFER_RECORD", record);
 		} catch (IOException e) {
 			logManager.logger.log(Level.SEVERE, e.getMessage());
 		}
@@ -273,10 +284,9 @@ public class DcmsServerImpl implements Dcms  {
 			req.join();
 			if (removeRecordAfterTransfer(recordID) == "success") {
 				System.out.println(recordsMap);
-				logManager.logger.log(Level.INFO, "Record created in  "
-						+ remoteCenterServerName + "  and removed from " + location);
-				return "Record created in " + remoteCenterServerName
-						+ "and removed from " + location;
+				logManager.logger.log(Level.INFO,
+						"Record created in  " + remoteCenterServerName + "  and removed from " + location);
+				return "Record created in " + remoteCenterServerName + "and removed from " + location;
 			}
 		} catch (Exception e) {
 
@@ -301,8 +311,7 @@ public class DcmsServerImpl implements Dcms  {
 	private synchronized Record getRecordForTransfer(String recordID) {
 		for (Entry<String, List<Record>> value : recordsMap.entrySet()) {
 			List<Record> mylist = value.getValue();
-			Optional<Record> record = mylist.stream()
-					.filter(x -> x.getRecordID().equals(recordID)).findFirst();
+			Optional<Record> record = mylist.stream().filter(x -> x.getRecordID().equals(recordID)).findFirst();
 			if (recordID.contains("TR")) {
 				if (record.isPresent())
 					return (Teacher) record.get();
@@ -321,15 +330,13 @@ public class DcmsServerImpl implements Dcms  {
 	 *            gets the newcourses value and adds to the list
 	 *
 	 */
-	//@WebMethod(exclude=true)
-	public synchronized List<String> putCoursesinList(String newvalue) {
-		String[] courses = newvalue.split("//");
-		ArrayList<String> courseList = new ArrayList<>();
-		for (String course : courses)
-			courseList.add(course);
-		return courseList;
-	}
-
+	// @WebMethod(exclude=true)
+	/*
+	 * public synchronized List<String> putCoursesinList(String newvalue) { String[]
+	 * courses = newvalue.split("//"); ArrayList<String> courseList = new
+	 * ArrayList<>(); for (String course : courses) courseList.add(course); return
+	 * courseList; }
+	 */
 	/**
 	 * The editSRRecord function performs the edit operation on the student record
 	 * and returns the appropriate message
@@ -345,32 +352,30 @@ public class DcmsServerImpl implements Dcms  {
 	 *            client
 	 */
 
-	private synchronized String editSRRecord(String maangerID, String recordID,
-			String fieldname, String newvalue) {
+	private synchronized String editSRRecord(String maangerID, String recordID, String fieldname, String newvalue) {
 		for (Entry<String, List<Record>> value : recordsMap.entrySet()) {
 			List<Record> mylist = value.getValue();
-			Optional<Record> record = mylist.stream()
-					.filter(x -> x.getRecordID().equals(recordID)).findFirst();
+			Optional<Record> record = mylist.stream().filter(x -> x.getRecordID().equals(recordID)).findFirst();
 			if (record.isPresent()) {
 				if (record.isPresent() && fieldname.equals("Status")) {
 					((Student) record.get()).setStatus(newvalue);
-					logManager.logger.log(Level.INFO,
-							maangerID + "Updated the records\t" + location);
+					logManager.logger.log(Level.INFO, maangerID + "Updated the records\t" + location);
 					return "Updated record with status :: " + newvalue;
 				} else if (record.isPresent() && fieldname.equals("StatusDate")) {
 					((Student) record.get()).setStatusDate(newvalue);
-					logManager.logger.log(Level.INFO,
-							maangerID + "Updated the records\t" + location);
+					logManager.logger.log(Level.INFO, maangerID + "Updated the records\t" + location);
 					return "Updated record with status date :: " + newvalue;
-				} else if (record.isPresent()
-						&& fieldname.equals("CoursesRegistered")) {
-					List<String> courseList = putCoursesinList(newvalue);
+				} else if (record.isPresent() && fieldname.equals("CoursesRegistered")) {
+					//List<String> courseList = _operations.putCoursesinList(newvalue);
+					String[] courses = newvalue.split("//");
+					ArrayList<String> courseList = new ArrayList<>();
+					for (String course : courses)
+						courseList.add(course);
 					((Student) record.get()).setCoursesRegistered(courseList);
 					return "Updated record with courses :: " + courseList;
 				} else {
 					System.out.println("Record with " + recordID + " not found");
-					logManager.logger.log(Level.INFO,
-							"Record with " + recordID + "not found!" + location);
+					logManager.logger.log(Level.INFO, "Record with " + recordID + "not found!" + location);
 					return "Record with " + recordID + " not found";
 				}
 			}
@@ -393,38 +398,32 @@ public class DcmsServerImpl implements Dcms  {
 	 *            client
 	 */
 
-	private synchronized String editTRRecord(String managerID, String recordID,
-			String fieldname, String newvalue) {
+	private synchronized String editTRRecord(String managerID, String recordID, String fieldname, String newvalue) {
 		for (Entry<String, List<Record>> val : recordsMap.entrySet()) {
 
 			List<Record> mylist = val.getValue();
-			Optional<Record> record = mylist.stream()
-					.filter(x -> x.getRecordID().equals(recordID)).findFirst();
+			Optional<Record> record = mylist.stream().filter(x -> x.getRecordID().equals(recordID)).findFirst();
 
 			if (record.isPresent()) {
 				if (record.isPresent() && fieldname.equals("Phone")) {
 					((Teacher) record.get()).setPhone(newvalue);
-					logManager.logger.log(Level.INFO,
-							managerID + "Updated the records\t" + location);
+					logManager.logger.log(Level.INFO, managerID + "Updated the records\t" + location);
 					return "Updated record with Phone :: " + newvalue;
 				}
 
 				else if (record.isPresent() && fieldname.equals("Address")) {
 					((Teacher) record.get()).setAddress(newvalue);
-					logManager.logger.log(Level.INFO,
-							managerID + "Updated the records\t" + location);
+					logManager.logger.log(Level.INFO, managerID + "Updated the records\t" + location);
 					return "Updated record with address :: " + newvalue;
 				}
 
 				else if (record.isPresent() && fieldname.equals("Location")) {
 					((Teacher) record.get()).setLocation(newvalue);
-					logManager.logger.log(Level.INFO,
-							managerID + "Updated the records\t" + location);
+					logManager.logger.log(Level.INFO, managerID + "Updated the records\t" + location);
 					return "Updated record with location :: " + newvalue;
 				} else {
 					System.out.println("Record with " + recordID + " not found");
-					logManager.logger.log(Level.INFO,
-							"Record with " + recordID + "not found!" + location);
+					logManager.logger.log(Level.INFO, "Record with " + recordID + "not found!" + location);
 					return "Record with " + recordID + " not found";
 				}
 			}
